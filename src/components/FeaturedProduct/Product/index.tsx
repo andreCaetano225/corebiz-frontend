3c30d1b0baead41c8d24cfde346582a487fff8b9
André de Sousa <andre.caetano2007@hotmail.com>
2025-01-17 16:53:21 -0300

    Pegando produtos listados, e aplicando no swiper para visualização de slider, ajustando para que os produtos fiquem centralizados | Taking listed products and applying them to the swiper for slider viewing, adjusting so that the products are centered

--- src/components/FeaturedProduct/Product/index.tsx
+++ src/components/FeaturedProduct/Product/index.tsx
@@ -1,4 +1,9 @@
+import StarRating from '../Star';
 import { ProductType } from './types';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper';
+import ArrowLeaft from '../../../assets/left-arrow.svg';
+import ArrowRight from '../../../assets/right-arrow.svg';
 
 const data = [
   {
@@ -52,37 +57,88 @@ const data = [
     price: 7990,
     installments: [],
   },
+  {
+    productId: 4,
+    productName: 'CINTO SEMICROMO PRETO 40MM',
+    stars: 3,
+    imageUrl: 'https://corebiz-test-server.onrender.com/images/product-4.png',
+    listPrice: null,
+    price: 7990,
+    installments: [],
+  },
+  {
+    productId: 4,
+    productName: 'CINTO SEMICROMO PRETO 40MM',
+    stars: 3,
+    imageUrl: 'https://corebiz-test-server.onrender.com/images/product-4.png',
+    listPrice: null,
+    price: 7990,
+    installments: [],
+  },
 ];
 
 export const Product = () => {
   return (
     <>
-      <div className="featuredProduct__direction-item">
-        {data.map((item: ProductType, index) => {
-          return (
-            <>
-              <div className="featuredProduct__item">
-                <img src={item.imageUrl} alt="" />
+      <Swiper
+        slidesPerView={4}
+        modules={[Navigation]}
+        navigation={{
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        }}
+        loop={true}
+      >
+        {data.map((item: ProductType, index) => (
+          <SwiperSlide key={item.productId}>
+            <div className="featuredProduct__item">
+              {item.listPrice ? (
+                <div className="featuredProduct__container">
+                  <strong>OFF</strong>
+                </div>
+              ) : null}
+
+              <img src={item.imageUrl} alt={item.productName} />
+              <div className="featuredProduct__div-item">
                 <span>{item.productName}</span>
-                {item.listPrice
-                  ? `de ${(item.listPrice / 100).toFixed(2).replace('.', ',')}`
-                  : null}
-                <p>por R$ {(item.price / 100).toFixed(2).replace('.', ',')}</p>
-                <p>
-                  {item.installments.length > 0 ? (
-                    <>
-                      ou em {item.installments[0].quantity}x de R${' '}
-                      {(item.installments[0].value / 100)
-                        .toFixed(2)
-                        .replace('.', ',')}
-                    </>
-                  ) : null}
-                </p>
+                <br />
+                <StarRating rating={item.stars} />
+                <div>
+                  <span>
+                    {item.listPrice
+                      ? `de R$ ${(item.listPrice / 100).toFixed(2).replace('.', ',')}`
+                      : ''}{' '}
+                  </span>
+
+                  <h3>
+                    por R$ {(item.price / 100).toFixed(2).replace('.', ',')}
+                  </h3>
+                  <p>
+                    {item.installments.length > 0 ? (
+                      <>
+                        ou em {item.installments[0].quantity}x de R${' '}
+                        {(item.installments[0].value / 100)
+                          .toFixed(2)
+                          .replace('.', ',')}
+                      </>
+                    ) : (
+                      ''
+                    )}
+                  </p>
+                  <button>COMPRAR</button>
+                </div>
               </div>
-            </>
-          );
-        })}
-      </div>
+            </div>
+          </SwiperSlide>
+        ))}
+
+        <div className="swiper-button-prev">
+          <img src={ArrowLeaft} alt="" />
+        </div>
+        <div className="swiper-button-next">
+          <img src={ArrowRight} alt="" />
+        </div>
+      </Swiper>
     </>
   );
 };
